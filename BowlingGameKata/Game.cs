7255fbefe53f7dcdb346fd227bbea7f8e66d0574
askalee <aska.lee@gmail.com>
2016-04-30 08:57:36 +0800

    first version for coding dojo

---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+
+        public static int GetScore(List<int> pins)
+        {
+            int score = 0;
+            for (int i = 0; i < pins.Count;i++ )
+            {
+                if (pins[i] == 10)
+                    score = pins[i] + pins[i + 2] + pins[i + 3];
+                else
+                    score += pins[i];
+            }
+
+            return score;
+        }
+    }
+}
---
+++ BowlingGameKata/TestBowling.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    [TestFixture]
+    public class TestBowling
+    {
+        [Test]
+        public void AllZero()
+        {
+            List<int> pins = new List<int> 
+            {
+                0,0,  0,0,  0,0,  0,0,  0,0,
+                0,0,  0,0,  0,0,  0,0,  0,0
+            };
+
+            Assert.That(Game.GetScore(pins), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AllOnes()
+        {
+            List<int> pins = new List<int> 
+            {
+                1,1,  1,1,  1,1,  1,1,  1,1, 
+                1,1,  1,1,  1,1,  1,1,  1,1 
+            };
+
+            Assert.That(Game.GetScore(pins), Is.EqualTo((1+1)*10));
+        }
+
+        [Test]
+        public void OneStrike()
+        {
+            List<int> pins = new List<int> 
+            {
+                10,0,  1,1,  1,1,  1,1,  1,1, 
+                1,1,   1,1,  1,1,  1,1,  1,1 
+            };
+
+            Assert.That(Game.GetScore(pins), Is.EqualTo(
+                (10 + 1 + 1) + (1 + 1)* 9));
+        }
+
+
+    }
+}
